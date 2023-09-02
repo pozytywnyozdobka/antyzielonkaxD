4f6ae26a30eb34eca2221e9a8651a1152d586ddb
pozytywnyozdobka <losiewskidominik@gmail.com>
2023-09-02 19:58:22 +0200

    Poprawki

--- XCDD/script.js
+++ XCDD/script.js
@@ -1,14 +1,18 @@
 function usuwanie() {
-    let zielonki = document.getElementsByClassName("username green-profile ");
+    console.log("XD")
+    let zielonki = document.getElementsByClassName("green-profile")
     if (zielonki.length > 0) {
         for (let i = 0; i < zielonki.length; i++) {
+            if (typeof(zielonki[i].closest(".entry")) != 'undefined' && zielonki[i].closest(".entry") != null) {
             zielonki[i].closest(".entry").style.display = "none"
-            zielonki[i].closest(".entry ").style.display = "none"
+            }
+
         }
     }
 
     let posty = document.getElementsByClassName("wrapper")
     for (let i = 0; i < posty.length; i++) {
+        if (typeof(posty[i].closest(".entry")) != 'undefined' && posty[i].closest(".entry") != null) {
         let post = posty[i].textContent.replace(/\s/g, '')
         let u = () => { posty[i].closest(".entry ").style.display = "none" }
         if (post.includes("strumyk")) {
@@ -20,36 +24,25 @@ function usuwanie() {
         if (post.includes("blazej")) {
             u()
         }
+    }
         
     }
 
 }
 
+document.addEventListener("load", usuwanie())
+
 
 
 
 const resizeObserver = new ResizeObserver(entries => {
-        if (entries[0].contentRect.height > 200) {
-            usuwanie()
-        }
+        setTimeout(usuwanie, 200)
 
     }
 )
 
-var target = document.querySelector('.tag-page');
-
-var observer = new MutationObserver(function(mutations) {
-  mutations.forEach(function(mutation) {
-    console.log(mutation.type);
-    usuwanie()
-  });
-});
-
-// configuration of the observer
-var config = { attributes: true, childList: true, characterData: true };
 
 
-observer.observe(target, config);
 
 resizeObserver.observe(document.getElementsByClassName("main-content")[0])
 
